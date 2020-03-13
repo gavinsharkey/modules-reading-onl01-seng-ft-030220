de2936a949171ca58daaab18d00f5989725c9b43
Gavin Sharkey <gavinsharkey2016@gmail.com>
2020-03-13 15:57:59 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,8 +1,9 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+require_relative './fancy_dance.rb'
 
 class Dancer
-  include Dance
+  include Fancy
   extend MetaDancing
 
   attr_accessor :name
